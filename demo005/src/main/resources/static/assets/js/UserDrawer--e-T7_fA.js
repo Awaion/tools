1e54e27a541ec131500e789a74e21d5871657846
Awaion <Awaion@qq.com>
2024-07-19 20:57:49 +0800

    demo005

--- demo005/src/main/resources/static/assets/js/UserDrawer--e-T7_fA.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./UserDrawer.vue_vue_type_script_setup_true_name_UserDrawer_lang-wPv5hSIs.js";import"./index-BbXpY4Iy.js";import"./Imgs-l7sbwhV1.js";import"./upload-LPSH1f05.js";import"./_plugin-vue_export-helper-x3n3nnut.js";export{o as default};
--- demo005/src/main/resources/static/assets/js/upload-LPSH1f05.js
+++
@@ -1,1 +0,0 @@
-import{aa as o,ab as e}from"./index-BbXpY4Iy.js";const s=a=>o.post(e+"/file/upload/img",a,{cancel:!1}),t=a=>o.post(e+"/file/upload/video",a,{cancel:!1});export{t as a,s as u};
